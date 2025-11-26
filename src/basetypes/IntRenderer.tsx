c0f9dac814d665cec4d538dccecbdca1f2fcf3df
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-26 14:43:54 +0100

    feat: int renderer inline min val max

--- src/basetypes/IntRenderer.tsx
+++ src/basetypes/IntRenderer.tsx
@@ -2,29 +2,33 @@ import { IntDataEntry } from 'url-safe-bitpacking/dist/types';
 
 export const IntRenderer: React.FC<{ d: IntDataEntry; onMutate: (d: IntDataEntry) => void }> = ({ d, onMutate }) => (
   <>
-    <span className="text-right">value</span>
-    <input
-      className="small"
-      type="number"
-      value={d.value}
-      min={d.min}
-      max={d.max}
-      step={1}
-      onChange={(e) => onMutate({ ...d, value: Number(e.target.value) })}
-    />
-    <span className="text-right">min</span>
-    <input
-      className="small"
-      type="number"
-      value={d.min}
-      onChange={(e) => onMutate({ ...d, min: Number(e.target.value) })}
-    />
-    <span className="text-right">max</span>
-    <input
-      className="small"
-      type="number"
-      value={d.max}
-      onChange={(e) => onMutate({ ...d, max: Number(e.target.value) })}
-    />
+    <span className="text-right font-bold">value</span>
+    <span className="flex flex-row gap-1 justify-between">
+      {'['}
+      <input
+        className="small w-25"
+        type="number"
+        value={d.min}
+        onChange={(e) => onMutate({ ...d, min: Number(e.target.value) })}
+      />
+      {'<'}
+      <input
+        className="small w-25"
+        type="number"
+        value={d.value}
+        min={d.min}
+        max={d.max}
+        step={1}
+        onChange={(e) => onMutate({ ...d, value: Number(e.target.value) })}
+      />
+      {'<'}
+      <input
+        className="small w-25"
+        type="number"
+        value={d.max}
+        onChange={(e) => onMutate({ ...d, max: Number(e.target.value) })}
+      />
+      {']'}
+    </span>
   </>
 );
