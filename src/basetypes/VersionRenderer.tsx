20ec76af9366230845a6233052d58f8e459a55db
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-26 14:44:40 +0100

    feat: one line version value

--- src/basetypes/VersionRenderer.tsx
+++ src/basetypes/VersionRenderer.tsx
@@ -5,21 +5,23 @@ export const VersionRenderer: React.FC<{
   onMutate: (d: VersionDataEntry) => void;
 }> = ({ d, onMutate }) => (
   <>
-    <span className="text-right">value</span>
-    <select className="small" value={d.value} onChange={(e) => onMutate({ ...d, value: Number(e.target.value) })}>
-      {[...Array(2 ** d.bits)].map((_, index) => (
-        <option key={index} value={index} children={index} />
-      ))}
-    </select>
-    <span className="text-right">bit count</span>
-    <select
-      className="small"
-      value={d.bits}
-      onChange={(e) => onMutate({ ...d, bits: Number(e.target.value) as VersionRangeType })}
-    >
-      {[4, 6, 8, 10].map((v, i) => (
-        <option key={i} value={v} children={v} />
-      ))}
-    </select>
+    <span className="text-right text-bold">value</span>
+    <span className="grid grid-cols-[1fr_auto_auto] gap-2">
+      <select className="small" value={d.value} onChange={(e) => onMutate({ ...d, value: Number(e.target.value) })}>
+        {[...Array(2 ** d.bits)].map((_, index) => (
+          <option key={index} value={index} children={index} />
+        ))}
+      </select>
+      <span className="text-right">bit count</span>
+      <select
+        className="small"
+        value={d.bits}
+        onChange={(e) => onMutate({ ...d, bits: Number(e.target.value) as VersionRangeType })}
+      >
+        {[4, 6, 8, 10].map((v, i) => (
+          <option key={i} value={v} children={v} />
+        ))}
+      </select>
+    </span>
   </>
 );
