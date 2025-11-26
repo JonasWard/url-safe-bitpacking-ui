5f93199ed99d441b085ad2e08620bf6772e2c071
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-26 17:51:47 +0100

    chore: no background for descriptor data, only padding

--- src/basetypes/shared/ComplexContentWrapper.tsx
+++ src/basetypes/shared/ComplexContentWrapper.tsx
@@ -22,7 +22,7 @@ export const ComplexContentWrapper: React.FC<ComplexContentWrapperProps> = ({ de
         {showDescriptor ? (
           <>
             <div className="absolute top-[-.55rem] left-[calc(100%+1rem)] h-0">
-              <div className="background justify-center flex flex-col gap-2">{descriptorContent}</div>
+              <div className=" py-2 justify-center flex flex-col gap-2">{descriptorContent}</div>
             </div>
             <div className="absolute top-[-2.65rem] left-[calc(100%+1rem)] h-0">
               <button className="w-2" onClick={() => setShowDescriptor(!showDescriptor)}>
