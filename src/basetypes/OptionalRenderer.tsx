39866dcbb81da2dca8a3152264a5512b62494f0b
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-26 18:48:38 +0100

    fix: descriptorParsing allows for toggling between both stages

--- src/basetypes/OptionalRenderer.tsx
+++ src/basetypes/OptionalRenderer.tsx
@@ -1,10 +1,24 @@
 import { NestedData, OptionalDataEntry } from 'url-safe-bitpacking/dist/types';
 import { OptionsRenderer } from './shared/OptionsRenderer';
 import { ComplexContentWrapper } from './shared/ComplexContentWrapper';
-import { getDefaultDescriptorForType } from '@/utils/defaults';
 
-const descriptorParsing = (d: (NestedData | null)[]): OptionalDataEntry['descriptor'] =>
-  d[0] && d[0].length > 0 ? [d[0], null] : [null, d[1] ?? [getDefaultDescriptorForType('INT', 'an int')]];
+const getValidStates = (d: OptionalDataEntry['descriptor']): [boolean, boolean] => [
+  !Boolean(d[0] && d[0].length > 0),
+  !Boolean(d[1] && d[1].length > 0)
+];
+
+const descriptorParsing = (
+  current: OptionalDataEntry['descriptor'],
+  d: (NestedData | null)[]
+): OptionalDataEntry['descriptor'] => {
+  const [c0, c1] = getValidStates(current);
+  const [n0, n1] = getValidStates(d as OptionalDataEntry['descriptor']);
+
+  if (n0 !== n1) return [n0 ? null : d[0], n1 ? null : d[1]] as OptionalDataEntry['descriptor'];
+  if (n0 === true) return [c0 ? current[1] : null, c1 ? current[0] : null] as OptionalDataEntry['descriptor'];
+  // if (n0 === false)
+  return [c0 ? null : current[0], c1 ? null : current[1]] as OptionalDataEntry['descriptor'];
+};
 
 const getDisabledMask = (d: OptionalDataEntry): boolean[] =>
   d.descriptor[0] && d.descriptor[0].length > 0 ? [false, true] : [true, false];
@@ -17,7 +31,7 @@ export const OptionalRenderer: React.FC<{ d: OptionalDataEntry; onMutate: (d: Op
     descriptorContent={
       <OptionsRenderer
         d={d.descriptor as (NestedData | null)[]}
-        setData={(e) => onMutate({ ...d, descriptor: descriptorParsing(e) })}
+        setData={(e) => onMutate({ ...d, descriptor: descriptorParsing(d.descriptor, e) })}
         disableEditAmount
         disabledMask={getDisabledMask(d)}
       />
